e5cd373d965f0928e0343a1fa6142b9614b8e2dd
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2025-12-04 21:06:42 -0500

    post works

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.get("/vehicles/", response_model=list[VehicleRead])
+def list_vehicles(db: Session = Depends(get_db)):
+    vehicles = db.query(models.Vehicle).all()
+    return vehicles
+
+@app.post("/vehicles/", response_model=VehicleRead, status_code=status.GTTP_201_CREATED)
+def 
